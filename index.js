7c85ca3368a50fee5c7ac9dcb66ed9fc6d36aa25
Cardaccio <juangomezpablo@gmail.com>
2022-06-21 13:09:40 -0300

    Styled css

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-let size = 8;
-let str = '';
+//Pasador de paginas
 
-for (let i = 0; i < size; i++) {
-    for (let y = 0; y < size; y++) {
-        if((i+y)%2==0){
-            str+=" ";
-        }else{
-            str +="#";
-        }
+const pages = document.querySelectorAll(".page");
+const translateAmount = 100;
+let translate = 0;
+slide = (direction) => {
+    direction === "next" ? translate -= translateAmount : translate += translateAmount;
+    pages.forEach(
+        pages => (pages.style.transform = `translateY(${translate}%)`),
+    );
+};
+
+//calculador prestamo
+
+let tipoCalc = 'prestamo';
+let tipoTime = "dias"
+let tiempo = 4;
+let amount = 20000;
+let interest = 10;
+let tasaCalc=((interest / 12) / 30);
+let iva = [1.21, 1.105, 1];
+let bigMac = 1200;
+let deudaTotal;
+let intDiario;
+let deudaDiaria;
+let deudaIva;
+let deudaMes;
+let ivaSaldo;
+let ivaInt;
+
+deudaDiaria= (((amount*interest)/100)+amount*iva[0])/tiempo;
+deudaMes = (((amount*interest)/100)+amount*iva[0])/tiempo;
+ivaInt = ((amount*interest)/100)*iva[0];
+deudaTotal=amount+ivaInt+ivaSaldo;
+ivaSaldo = amount*iva;
+
+
+let totalBigMac = deudaTotalInt/bigMac;
+
+console.log(deudaTotal)
+
+if (tipoCalc == 'prestamo') {
+    if (tipoTime == 'dias') {
+        deuda = tasaCalc * tiempo;
+
+    } else if (tipoTime == 'meses') {
+        deuda = (tasaCalc * 30) * tiempo;
+
+
+    } else {
+        alert('Te falta elegir el tipo de operacion a tomar')
     }
-    str += "\n";
+
 }
 
-
-console.log(str);
